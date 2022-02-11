92118c4a7b617319a082943969c25c74573d4053
Ridhima Mehrotra <rm8970@srmist.edu.in>
2022-02-11 22:15:35 +0530

    stopwatch first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use-script";
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const hours = document.querySelector(".hours");
+start.addEventListener("click", () => {});
